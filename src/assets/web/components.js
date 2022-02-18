489f751a86dc85013aabccb4c51bb2e870c24ef0
Simon Brulhart <simon@brulhart.me>
2022-02-18 20:42:23 +0100

    Move template into scripts file
    Use VSCode extension "Javascript Vue" for coloration.

--- src/assets/web/components.js
+++ src/assets/web/components.js
@@ -1,5 +1,5 @@
 // TODO: Use prod module in "release" mode ?
-import { createApp } from "/static/vendor/vue@3.0.9/vue.esm-browser.js";
+import { createApp } from "/static/vendor/vue@3.2.31/vue.esm-browser.js";
 
 export function createMainApp() {
   const app = createApp(App);
@@ -11,7 +11,15 @@ export function createMainApp() {
 
 export const App = {
   name: "App",
-  template: "#App",
+  template: `
+    <h1>Spotify Search</h1>
+    <div>
+      <form @submit.prevent="fetchResults(form.query)">
+        <input v-model="form.query" placeholder="Search terms..." class="search-input">
+      </form>
+    </div>
+    <SearchResults :items="results.items"/>
+  `,
   data() {
     return {
       form: {
@@ -41,7 +49,14 @@ export const Label = {
 
 export const SearchResults = {
   name: "SearchResults",
-  template: "#SearchResults",
+  template: `
+    <div class="container mrgt+">
+      <Label>Search results</Label>
+      <div class="mrgt+">
+        <SearchResult v-if="items" v-for="item in items" v-bind="item" class="row"/>
+      </div>
+    </div>
+  `,
   props: {
     items: Array,
   },
@@ -49,7 +64,14 @@ export const SearchResults = {
 
 export const SearchResult = {
   name: "SearchResult",
-  template: "#SearchResult",
+  template: `
+    <div class="row">
+      <div class="cell"><img :src="thumbnail_url" height="48" width="48"></div>
+      <div class="cell"><a :href="uri">{{ title }}</a></div>
+      <div class="cell">{{ artistsLabel }}</div>
+      <div class="cell">{{ collection }}</div>
+    </div>
+  `,
   props: {
     title: String,
     artists: Array,
